a47686d4a2b5a01a944cc3560fb203f68f117124
Patrick Dutra <patrickdutra100@hotmail.com>
2022-11-07 18:40:43 -0300

    Frist Commit

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 
 const Body = styled.body`
-
     width: 100%;
     height: 250%;
-    
 `;
 
 export default Body;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,27 @@
 import { width } from "@mui/system";
-// import Olho from "./images/olho.jpg"
+import Olho from "../images/gato.jpg";
 import React from "react";
 import Body from "../components/Body/Body";
-import TvIcon from '@mui/icons-material/Tv';
+import TvIcon from "@mui/icons-material/Tv";
 
 const Home: React.FC = () => {
-    return (     
-        
+    return (
         <Body>
-        <div>
-            {/* <img src={Olho} style={{width:"100px", height:"100px"}}/> */}
-            <img src="./images/gato.jpg" style={{width:"100%", height:"200px"}}/>
-            </div >
+            <div>
+                <img
+                    src={Olho}
+                    style={{
+                        width: "100vh",
+                        height: "200px",
+                        border: "7px, red",
+                    }}
+                />
+            </div>
             <div>
                 <TvIcon></TvIcon>
             </div>
         </Body>
-
-    );    
+    );
 };
 
 export default Home;
